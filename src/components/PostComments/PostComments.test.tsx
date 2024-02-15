7f350bcdf3c1f5ccaa7449b45bb89ac61c151dbc
Mauricio Sousa <mauriciosouza8989@gmail.com>
2024-02-14 21:00:00 -0300

    finalizando exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+import modelComments from '../../models/Comment'
+
+function addComment(comment: modelComments){
+    fireEvent.change(screen.getByTestId('textarea'),{
+        target: {
+            value: comment.comment
+        }
+    });
+    fireEvent.click(screen.getByTestId('btn-add-comment'))
+    expect(screen.getByText('teste 1')).toBeInTheDocument()
+}
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        const {debug} = render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+        addComment({ id: 1, comment: 'teste 1'})
+        addComment({ id: 2, comment: 'teste 2'})
+        debug()
     });
 });
