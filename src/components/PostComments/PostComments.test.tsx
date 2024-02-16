1e90b5369d9a8f4e66bd2ba8cbf8bc7506d68f23
Mauricio Sousa <mauriciosouza8989@gmail.com>
2024-02-16 18:34:31 -0300

    adicionando arquivo de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -9,15 +9,13 @@ function addComment(comment: modelComments){
         }
     });
     fireEvent.click(screen.getByTestId('btn-add-comment'))
-    expect(screen.getByText('teste 1')).toBeInTheDocument()
 }
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        const {debug} = render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        render(<PostComment/>);
         addComment({ id: 1, comment: 'teste 1'})
         addComment({ id: 2, comment: 'teste 2'})
-        debug()
+        expect(screen.getByText(/teste 2/i)).toBeInTheDocument()
     });
 });
